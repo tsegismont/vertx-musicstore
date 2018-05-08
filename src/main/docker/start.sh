1bf7ff0bf30b1fee25fe4f4d57de0309c0d25eb9
Thomas Segismont <tsegismo@redhat.com>
2018-05-08 18:54:21 -0300

    Remove logback, to reduce the amount of config shown

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -5,4 +5,4 @@ sleep 10
 curl -X POST -u Administrator:password -d name=album-comments -d ramQuotaMB=200 -d authType=none -d proxyPort=11315 \
 http://couchbase:8091/pools/default/buckets
 
-exec java -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -Dvertx.disableDnsResolver=true -jar ${APP_FILE} -conf ${APP_CONF}
+exec java -Dvertx.disableDnsResolver=true -jar ${APP_FILE} -conf ${APP_CONF}
